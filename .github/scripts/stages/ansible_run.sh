e0accdfb3cf2b5f411bcaf20931f366bc63a4be8
JoseStud <jariza@utb.edu.co>
2026-03-16 18:04:57 -0500

    fix: add ANSIBLE_ROLES_PATH variable to ansible_run.sh for role path configuration

--- .github/scripts/stages/ansible_run.sh
+++ .github/scripts/stages/ansible_run.sh
@@ -21,6 +21,7 @@ if [[ -n "${ANSIBLE_TAGS}" ]]; then
 fi
 
 ANSIBLE_TIMEOUT="${ANSIBLE_TIMEOUT:-30}" \
+ANSIBLE_ROLES_PATH=ansible/roles \
 ANSIBLE_SSH_ARGS='-o StrictHostKeyChecking=accept-new -o CertificateFile=~/.ssh/id_ed25519-cert.pub -i ~/.ssh/id_ed25519' \
   infisical run --projectId="${INFISICAL_PROJECT_ID}" --env=prod -- \
   ansible-playbook "${ansible_args[@]}"
